e4d22a072dddec60064d244d87ec0f57d5ada476
Helmer7 <vhelmert@icloud.com>
2024-10-14 17:05:41 -0300

    Adicionando arquivos após limpar cache

--- Desktop/LTIV3/lti_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.lti_login, name='lti_login'),
-    path('launch/', views.lti_launch, name='lti_launch'),
-    path('jwks/', views.jwks, name='jwks'),
-]
