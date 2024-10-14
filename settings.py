72cf7e3e9a3279dfe904b8cfa3dfb4d4630b0601
Helmer7 <vhelmert@icloud.com>
2024-10-14 17:57:51 -0300

    Adicionando arquivos após limpar cache

---
+++ settings.py
@@ -0,0 +1,103 @@
+import os
+from pathlib import Path
+
+# Caminho base do projeto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Chave secreta
+SECRET_KEY = 'c5ym#@d+qll!ii8uk6si*a30@mm3hiyh)biscf%cq8^38zlf_u'
+
+# Modo de Debug (desative em produção)
+DEBUG = True
+
+# Hosts permitidos (altere isso para incluir o domínio da sua aplicação)
+ALLOWED_HOSTS = ['*']
+
+# Aplicações instaladas
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Adicione aqui seu app LTI, por exemplo:
+    'lti_tool',
+]
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# URL de configuração do projeto
+ROOT_URLCONF = 'lti_tool.urls'
+
+# Templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# WSGI application
+WSGI_APPLICATION = 'lti_tool.wsgi.application'
+
+# Banco de dados (aqui está configurado com o SQLite, mas você pode ajustar para o banco de dados que preferir)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Validação de senhas
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internacionalização
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Arquivos estáticos
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Arquivos de mídia (se necessário)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
